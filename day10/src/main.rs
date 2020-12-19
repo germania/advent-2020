1542d25066d7dadf7288783b75b758f572216bf2
Ben Clarke <me@benclarke.ca>
2020-12-18 22:49:10 -0500

    fmt and clippy

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,12 +1,11 @@
 use util::read_arg_file;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     let mut adapters: Vec<u64> = read_arg_file()?
         .map(|x| x.unwrap().parse::<u64>().unwrap())
         .collect();
 
-    &adapters.sort();
+    adapters.sort();
 
     let mut incrementsby1 = 1;
     let mut incrementsby3 = 1;
@@ -16,7 +15,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         match x - y {
             1 => incrementsby1 += 1,
             3 => incrementsby3 += 1,
-            _ => ()
+            _ => (),
         }
     }
 
@@ -25,5 +24,4 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("{}x{}={}", incrementsby1, incrementsby3, answer);
 
     Ok(())
-
 }
